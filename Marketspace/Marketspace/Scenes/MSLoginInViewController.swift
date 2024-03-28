dc293353e13c045aa730f209fa67017bd9c4748a
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-03-28 16:48:44 -0300

    project struct

---
+++ Marketspace/Marketspace/Scenes/MSLoginInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Marketspace
+//
+//  Created by Grazi  Berti on 28/03/24.
+//
+
+import UIKit
+
+class MSLoginInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
---
+++ Marketspace/Marketspace/Scenes/MSRegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MSRegisterViewController.swift
+//  Marketspace
+//
+//  Created by Grazi  Berti on 28/03/24.
+//
+
+import UIKit
+
+class MSRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
